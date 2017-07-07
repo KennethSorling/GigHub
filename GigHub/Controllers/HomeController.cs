2458b57c3e660d50625a9d51ecd42b70838eae70
Kenneth Sörling <kenneth.sorling@gmail.com>
2017-07-07 10:05:02 +0200

    Added Name property to Account + moved View Models to their own files and  the right namespace.

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -3,14 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GigHub.Models;
+
 
 namespace GigHub.Controllers
 {
+    using System.Data.Entity;
+
     public class HomeController : Controller
     {
+        ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new Models.ApplicationDbContext();
+        }
+
         public ActionResult Index()
         {
-            return View();
+            var upcomingGigs = _context.Gigs
+                .Where(g => g.DateTime > DateTime.Now)
+                .OrderBy(g => g.DateTime)
+                .Include(g => g.Artist);
+
+            return View(upcomingGigs);
         }
 
         public ActionResult About()
